3ca6ba8103ed9fa47f19ef6d0770aed9829a8b51
IyeddAmri <iyedamri21@gmail.com>
2024-02-26 10:56:20 +0100

    products crud done

---
+++ Back-End/routes/campingRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/campingControllers');
+
+
+router.get('/get', productController.getAllProducts);
+router.get('/products/:id', productController.getProductById);
+router.post('/', productController.createProduct);
+router.put('/products/:id', productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
+
+module.exports = router;
---
+++ Front-End/Screens/HomeScreen.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+import axios from 'axios';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the backend using Axios
+    axios.get('http://localhost:5000/api/get')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  return (
+    <View>
+      <Text>Product List</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.Name}
+        renderItem={({ item }) => (
+          <View>
+            <Image source={{ uri: item.ImageURL }} style={{ width: 100, height: 100 }} />
+            <Text>{item.Name}</Text>
+            <Text>{item.Description}</Text>
+            <Text>Price: ${item.Price.toFixed(2)}</Text>
+            <Text>Availability: {item.Availability ? 'Available' : 'Not Available'}</Text>
+            <Text>Category: {item.Category}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default App;

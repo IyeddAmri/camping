3cb87275e0cb3c7603c71b70d04b1546cf0455d6
JasserGa <jessergafsi2@gmail.com>
2024-02-26 16:51:59 +0300

    ...

--- Front-End/App.js
+++ Front-End/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './Screens/HomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>iyed</Text>
+      <HomePage/>
       <StatusBar style="auto" />
     </View>
   );

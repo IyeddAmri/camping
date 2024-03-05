7092afae796112113d74de9c0a798d251af51d0b
Roudaina-Jbeli <jbeliroudaina@gmail.com>
2024-03-05 22:51:38 +0100

    done

--- Back-End/routes/whishlistroute.js
+++ Back-End/routes/whishlistroute.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const wishlistController = require('../controllers/whishlistcontroller');
 
-// Add to wishlist
 router.post('/', wishlistController.addToWishlist);
-
-// Remove from wishlist
 router.delete('/:id', wishlistController.removeFromWishlist);
-
-// Get wishlist by user ID
 router.get('/:id', wishlistController.getWishlistByUserID);
 router.get('/get', wishlistController.getWishlist);
 

5c5ae8512e6b7a60614d2de86497a7a6899f96d6
IyeddAmri <iyedamri21@gmail.com>
2024-03-22 11:06:32 +0100

    f

--- Front-End/Screens/Checklist.js
+++ Front-End/Screens/Checklist.js
@@ -143,7 +143,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'cente',
   },
   modalContent: {
     backgroundColor: '#fff',

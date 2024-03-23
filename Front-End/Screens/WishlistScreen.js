06e3d638102a329d3e59cede7070563cd5a04b52
firaslamouchi21 <firaslamou@gmail.com>
2024-03-23 11:06:21 +0100

    425

--- Front-End/Screens/WishlistScreen.js
+++ Front-End/Screens/WishlistScreen.js
@@ -3,3 +3,11 @@ import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 
 const WishlistScreen = ({ route }) => {
   const wishlist = route.params?.wishlist || [];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.wishlistItem}>
+      <Text style={styles.itemText}>{item.LocationName}</Text>
+      <Image source={{ uri: item.imageUrl }} style={styles.image} />
+      <Text style={styles.itemText}>{item.Description}</Text>
+    </View>
+  );

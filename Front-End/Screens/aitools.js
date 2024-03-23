491709a00f9d6cfdf6723264081aeea0e76bb1e2
firaslamouchi21 <firaslamou@gmail.com>
2024-03-23 11:01:37 +0100

    417

--- Front-End/Screens/aitools.js
+++ Front-End/Screens/aitools.js
@@ -13,4 +13,18 @@ export function ObjectDetector() {
   const detectObjectsOnImage = async (imageUri) => {
     const imageTensor = await loadImageTensor(imageUri);
     const model = await cocoSsd.load();
-    const predictions = awai
+    const predictions = await model.detect(imageTensor);
+    setPredictions(predictions);
+    console.log("Predictions: ", predictions);
+    setLoading(false);
+  };
+
+  const loadImageTensor = async (imageUri) => {
+    const imgB64 = await FileSystem.readAsStringAsync(imageUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
+    const raw = new Uint8Array(imgBuffer);
+    const imageTensor = tf.node.decodeImage(new Uint8Array(raw), 3);
+    return imageTensor;
+  };

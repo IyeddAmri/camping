c5242682b17ea2c20ad1218ff34f3bae6a67fa28
firaslamouchi21 <firaslamou@gmail.com>
2024-03-23 11:10:58 +0100

    433

--- Front-End/config/googleSignIn.js
+++ Front-End/config/googleSignIn.js
@@ -2,17 +2,3 @@ import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../config/firebase'; 
 
 const GoogleSignIn = async () => {
-  try {
-    
-    const provider = new GoogleAuthProvider();
-    
-    const result = await signInWithPopup(auth, provider);
-
-  
-    return result.user;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export { GoogleSignIn };

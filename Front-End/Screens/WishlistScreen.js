8402ddd17ff738c5fe8909e4e3ff831ae1bb4fdd
firaslamouchi21 <firaslamou@gmail.com>
2024-03-23 10:59:47 +0100

    414

--- Front-End/Screens/WishlistScreen.js
+++ Front-End/Screens/WishlistScreen.js
@@ -4,20 +4,23 @@ import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 const WishlistScreen = ({ route }) => {
   const wishlist = route.params?.wishlist || [];
 
+  const renderItem = ({ item }) => (
+    <View style={styles.wishlistItem}>
+      <Text style={styles.itemText}>{item.LocationName}</Text>
+      <Image source={{ uri: item.imageUrl }} style={styles.image} />
+      <Text style={styles.itemText}>{item.Description}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Wishlist</Text>
       <FlatList
-  data={wishlist}
-  keyExtractor={(item) => item.Name}
-  renderItem={({ item }) => (
-    <View style={styles.wishlistItem}>
-      <Text>{item.LocationName}</Text>
-      <Image source={{ uri: item.imageUrl }} style={styles.image} />
-      <Text>{item.Description}</Text>
-    </View>
-  )}
-/>
+        data={wishlist}
+        keyExtractor={(item) => item.Name}
+        renderItem={renderItem}
+        contentContainerStyle={styles.flatListContent}
+      />
     </View>
   );
 };
@@ -33,15 +36,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
+  flatListContent: {
+    paddingBottom: 20,
+  },
   wishlistItem: {
+    marginBottom: 20,
+  },
+  itemText: {
     marginBottom: 10,
   },
   image: {
     width: '100%',
     height: 200, // Adjust the height as needed
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-});
-
-export default WishlistScreen;
+    bord
